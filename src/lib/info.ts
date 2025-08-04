62bc44476c4152309d9c4fbaeef67164c864ecb5
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-08-05 00:15:31 +0200

    feat: Introduce knowledge info cards and improve ticket display

---
+++ src/lib/info.ts
@@ -0,0 +1,32 @@
+export const infoCards = [
+  {
+    id: 1,
+    title: "Soft skills",
+    img: "",
+  },
+  {
+    id: 2,
+    title: "Frontend",
+    img: "",
+  },
+  {
+    id: 3,
+    title: "Backend",
+    img: "",
+  },
+  {
+    id: 4,
+    title: "Diseño",
+    img: "",
+  },
+  {
+    id: 5,
+    title: "QA",
+    img: "",
+  },
+  {
+    id: 6,
+    title: "Devops",
+    img: "",
+  },
+];
