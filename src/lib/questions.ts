5e44e28b1d328064c6d03f703e70f827b6df1316
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-08-05 23:19:27 +0200

    feat: Add frequent questions section and update CTA and Talks components
    Introduces a new 'FrequentQuestions' component that displays a list of frequently asked questions. Also updates the 'CTA' component to include decorative images and modifies the 'Talks' component header to include "and workshops".

---
+++ src/lib/questions.ts
@@ -0,0 +1,22 @@
+export const questions = [
+  {
+    id: 0,
+    question: "¿Dónde se podrá ver el evento?",
+    answer: "El evento se llevará a cabo en el canal twitch.tv/afor_digital",
+  },
+  {
+    id: 1,
+    question: "¿Tengo que estar suscrito al canal para ver el evento?",
+    answer: "El evento se llevará a cabo en el canal twitch.tv/afor_digital",
+  },
+  {
+    id: 2,
+    question: "¿Recibiré un pago por dar la charla?",
+    answer: "El evento se llevará a cabo en el canal twitch.tv/afor_digital",
+  },
+  {
+    id: 3,
+    question: "¿Podré ver las charlas en diferido?",
+    answer: "El evento se llevará a cabo en el canal twitch.tv/afor_digital",
+  },
+];
