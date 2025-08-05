b8206255f0d52852a23dab386cbc91460b7981b4
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-08-05 22:30:45 +0200

    feat: Add Talks section with schedule and speaker information
    Introduces a new section to display talks, including speaker details, times, and breaks. This involves creating new components for individual talks and the main talks section, and defining the talk data in a new TypeScript file. Also includes image assets for the talks section.

---
+++ src/lib/talks.ts
@@ -0,0 +1,39 @@
+export const talks = [
+  {
+    id: 0,
+    title: "API-first: Cómo diseñar backends pensando en frontend y QA",
+    img: "/imgs/question.webp",
+    author: "Jarrison Cano",
+    hour: "20.00h",
+  },
+  {
+    id: 1,
+    hasBreak: true,
+    title: "Pequeños detalles, gran impacto: UX microinteractions",
+    img: "/imgs/question.webp",
+    author: "Joseph aka xq",
+    hour: "20.30h",
+  },
+  {
+    id: 2,
+    title: "QA desde el primer commit: Testing para todos",
+    img: "/imgs/question.webp",
+    author: "Laura Nicole",
+    hour: "21.30h",
+  },
+  {
+    id: 3,
+    hasBreak: true,
+    title: "Frontend moderno con React + Tailwind + Shadcn",
+    img: "/imgs/question.webp",
+    author: "Felixicaza",
+    hour: "22.00h",
+  },
+  {
+    id: 4,
+    title: "Diseña como un sistema: Introducción a Design Systems",
+    img: "/imgs/question.webp",
+    author: "Cristina aka krepsi",
+    hour: "23.00h",
+  },
+];
