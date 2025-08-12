51ebd39714063f2152a740175dc4067f5bda9ea5
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-08-12 23:37:33 +0200

    feat: Add contributors section and SVGs
    Introduces SVG avatars for contributors and a new section in the Footer component to display them. Updates TicketAtropo component to reference new contributor data and renames a class for clarity. Adds a contributors list to `src/lib/contributors.ts`.

---
+++ src/lib/contributors.ts
@@ -0,0 +1,44 @@
+export const contributors = [
+  {
+    id: 0,
+    user: "aforcita",
+    img: "/contributors/aforcita.svg",
+    github_url: "https://github.com/afordigital",
+  },
+  {
+    id: 1,
+    title: "Felix Icaza",
+    img: "/contributors/aforcita.svg",
+    github_url: "https://github.com/felixicaza",
+  },
+  {
+    id: 2,
+    title: "Freilyn Bernabe",
+    img: "/contributors/aforcita.svg",
+    github_url: "https://github.com/bernabedev",
+  },
+  {
+    id: 3,
+    title: "Cristina Díaz",
+    img: "/contributors/aforcita.svg",
+    github_url: "https://github.com/Krepssier",
+  },
+  {
+    id: 4,
+    title: "Jhon",
+    img: "/contributors/jhon.svg",
+    github_url: "https://github.com/vyers",
+  },
+  {
+    id: 5,
+    title: "Mateo Morales",
+    img: "/contributors/vimofama.svg",
+    github_url: "https://github.com/vimofama",
+  },
+  {
+    id: 6,
+    title: "Leonardo Salazar",
+    img: "/contributors/aforcita.svg",
+    github_url: "https://github.com/developerleonardo",
+  },
+];
